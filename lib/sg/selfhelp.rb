6e0a2e0992e5fb2fe4622a9d3553b0fabe5bd4e1
Nolan Eakins <sneakin@semanticgap.com>
2023-10-31 03:40:06 -0400

    Factored out the command scanning.

--- lib/sg/selfhelp.rb
+++ lib/sg/selfhelp.rb
@@ -2,6 +2,27 @@ require 'sg/terminstry'
 
 module SG
   module SelfHelp
+    def self.scan_for_commands
+      File.open($0, 'rt') do |src|
+        broke = src.each_line do |l|
+          break true if l.match?(/\$0 == __FILE__/)
+        end
+        return [] unless broke
+
+        cmds = []
+        src.each_line do |l|
+          case l
+          when /^  when \/(.+)\/ then( # (.*$))?/,
+               /^  when '(.*)' then( # (.*$))?/,
+               /^  when "(.*)" then( # (.*$))?/
+          then
+            cmds << [ $1, $3 ]
+          end
+        end
+        return cmds
+      end
+    end
+    
     def self.print
       tty = SG::Terminstry::Terminals.global
       heading = tty.fg(SG::Color::VT100.new(:brightcyan)) + tty.bold + tty.italic
@@ -10,21 +31,8 @@ module SG
       puts("%sUsage:%s %s command [args...]" % [ heading, normal, $0 ])
       puts
       puts("%sCommands:%s" % [ heading, normal ])
-      File.open($0, 'rt') do |src|
-        broke = src.each_line do |l|
-          break true if l.match?(/\$0 == __FILE__/)
-        end
-        exit(-1) unless broke
-        cmds = []
-        src.each_line do |l|
-          case l
-          when /^  when \/(.+)\/ then( # (.*$))?/, /^  when '(.*)' then( # (.*$))?/ then
-            cmds << [ $1, $3 ]
-          end
-        end
-        cmds.each do |cmd, desc|
-          puts("%s%16s%s  %s" % [ bold, cmd, normal, desc ])
-        end
+      scan_for_commands.each do |(cmd, desc)|
+        puts("%s%16s%s  %s" % [ bold, cmd, normal, desc ])
       end
     end
   end
