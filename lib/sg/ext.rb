f8a6fe15f5fa42ce762a2158aee5dee5b5fbaa9e
Nolan Eakins <sneakin@semanticgap.com>
2023-11-15 16:25:43 -0500

    Needed to add the requiring of ext/false.

--- lib/sg/ext.rb
+++ lib/sg/ext.rb
@@ -6,6 +6,7 @@ end
 require_relative 'ext/class'
 require_relative 'ext/object'
 require_relative 'ext/nil'
+require_relative 'ext/false'
 require_relative 'ext/module'
 require_relative 'ext/string'
 require_relative 'ext/enum'
