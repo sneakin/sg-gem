0447a557b2a54566dca03c4215b45e7770a488a8
Nolan Eakins <sneakin@semanticgap.com>
2024-08-23 04:42:54 -0400

    Big rework on Unit. Still needs to be able to simplify units and dimensions.

--- lib/sg/units.rb
+++ lib/sg/units.rb
@@ -1,12 +1,18 @@
 # coding: utf-8
 require 'sg/converter'
 require 'sg/ext'
-require 'sg/algebra'
 
 using SG::Ext
 
 module SG::Units
+  class DimensionMismatch < TypeError
+    def initialize a, b
+      super("#{a.inspect} != #{b.inspect}")
+    end
+  end
+  
   class Unit
+    include Comparable
     include SG::Convertible
 
     attr_accessor :value
@@ -16,9 +22,16 @@ module SG::Units
     end
 
     def to_s
-      "%s %s" % [ value_string, abbrev ? abbrev : (value.abs <= 1 ? name : name.pluralize) ]
+      "%s %s" %
+        [ value_string,
+          abbrev ? abbrev : (value.abs <= 1 ? name : name.pluralize)
+        ]
     end
 
+    def inspect
+      "#<%s: %s>" % [ name, value ]
+    end
+    
     def name
       self.class.name
     end
@@ -32,7 +45,7 @@ module SG::Units
     end
 
     def coerce other
-      [ Unit.new(other), self ]
+      [ Unitless.new(other), self ]
     end
     
     class << self
@@ -50,113 +63,251 @@ module SG::Units
       end
 
       def derive name, abbrev = nil, dimension = nil
-        k = Class.new(self)
-        k.name = name
-        k.abbrev = abbrev
-        k.dimension = dimension || self.dimension
-        k
-      end      
+        Class.new(self).tap do |u|
+          u.name = name
+          u.abbrev = abbrev
+          u.dimension = dimension || self.dimension || Dimension.new(name)
+        end
+      end
+
+      def inspect
+        "#<Unit:%s:%s>" % [ dimension&.name, name ]
+      end
+
+      def eql? other
+        dimension == other.dimension &&
+          name == other.name &&
+          abbrev == other.abbrev
+      rescue NoMethodError
+        false
+      end
     end
-    
+
     class Per < Unit
       class << self
         attr_accessor :numerator
         attr_accessor :denominator
         def name
-          "%s / %s" % [ numerator.name, denominator.name ]
+          "%s / %s" % [ numerator.name.blank? ? '1' : numerator.name,
+                        denominator.name.blank? ? '1' : denominator.name
+                      ]
         end
 
         def abbrev
-          "%s/%s" % [ numerator.abbrev, denominator.abbrev ]
+          "%s/%s" % [ numerator.abbrev.blank? ? '1' : numerator.abbrev,
+                      denominator.abbrev.blank? ? '1' : denominator.abbrev
+                      ]
         end
         
         def dimension
-          nil
+          unless @dimension
+            nd = (Unit === numerator) ? numerator.dimension : NullDimension
+            dd = (Unit === denominator) ? denominator.dimension : NullDimension
+            @dimension ||= (nd == dd) ? NullDimension : nd / dd
+          end
+
+          @dimension
         end
+
+        def * other
+          # a/b * b => a
+          return numerator if denominator == other
+          # a/b * x/a => x/b
+          return other.numerator / denominator if other.superclass == Per && numerator == other.denominator
+          super(other)
+        end
+
+        def / other
+          # a/b / 1/b => a
+          return numerator / other.numerator if other.superclass == Per && denominator == other.denominator
+          # a/b / a => a/b * 1/a => 1/b
+          return Unitless / denominator if numerator == other
+          # a/b / a/x => a/b * x/a => x/b
+          return other.numerator / denominator if other.superclass == Per && numerator == other.numerator
+          super(other)
+        end
+
+        def invert
+          @invert ||= numerator == Unitless ? denominator : derive(denominator, numerator)
+        end
+        
+        def derive n, d
+          Class.new(Per).tap do |per|
+            per.numerator = n
+            per.denominator = d
+          end
+        end
+
       end
     end
-    
+
     def self./ other
-      k = Class.new(Per)
-      k.numerator = self
-      k.denominator = other
-      k
+      return Unitless if self == other
+      return self if other == 1 || other == Unitless
+      # A / A/x => x
+      return other.denominator if other.superclass == Per && self == other.numerator
+      # A / Ab => 1 / b
+      return Per.derive(Unitless, other.base) if Product === other && self == other.reps
+      return Per.derive(Unitless, other.reps) if Product === other && self == other.base
+      
+      this = self
+      @per_cache ||= Hash.new do |h, k|
+        h[k] = Per.derive(this, k)
+      end
+      @per_cache[other]
     end
 
+    def self.invert
+      @invert ||= Per.derive(Unitless, self)
+    end
+    
     class Product < Unit
       class << self
         attr_accessor :base
         attr_accessor :reps
 
         def name
-          super || "%s*%s" % [ base.name, reps.name ]
+          super || [ base.name, reps.name ].reject(&:blank?).join('*')
         end
 
         def abbrev
-          super || "%s*%s" % [ base.abbrev, reps.abbrev ]
+          super || [ base.abbrev, reps.abbrev ].reject(&:blank?).join('*')
         end
 
         def dimension
-          nil
+          @dimension ||= base.dimension * reps.dimension
+        end
+
+        def / other
+          # a*b / b => a
+          return base if reps == other
+          # a*b / a => b
+          return reps if base == other
+          # a*b / b/x => a/x
+          return base / other.denominator if other.superclass == Per && reps == other.numerater
+          # a*b / a/x => b/x
+          return reps / other.denominator if other.superclass == Per && base == other.numerater
+          
+          super(other)
+        end
+
+        def * other
+          # a*b * x/a => x*b
+          return other.numerator * reps if other.superclass == Per && base == other.denominator
+          # a*b * x/b => a*x
+          return base * other.numerator if other.superclass == Per && reps == other.denominator
+          super(other)
+        end
+
+        def derive base, reps
+          Class.new(Product).tap do |p|
+            p.base = base
+            p.reps = reps
+          end
         end
       end
     end
     
     def self.* other
-      k = Class.new(Product)
-      k.base = self
-      k.reps = other
-      k
+      return self if other == 1 || other == Unitless
+      return other.numerator if other.superclass == Per && self == other.denominator
+      
+      this = self
+      @product_cache ||= Hash.new do |h, k|
+        h[k] = Product.derive(this, k)
+      end
+      @product_cache[other]
     end
 
+    def same_dimension? other
+      Unit === other &&
+        self.class.dimension == other.class.dimension
+    end
+
+    def check_dimension! other
+      if Unit === other
+        if !same_dimension?(other)
+          raise DimensionMismatch.new(self.class.dimension, other.class.dimension)
+        end
+      else
+        raise DimensionMismatch.new(self.class.dimension, other.class)
+      end
+    end
+
+    def nan?
+      value.nan?
+    end
+    
     def abs
       self.class.new(value.abs)
     end
     
-    def + other
-      other = other.to(self.class)
-      self.class.new(value + other.value)
+    def +@
+      self
     end
-
+    
     def -@
       self.class.new(-value)
     end
     
+    def + other
+      check_dimension!(other)
+      other = other.to(self.class)
+      self.class.new(value + other.value)
+    end
+
     def - other
+      check_dimension!(other)
       other = other.to(self.class)
       self.class.new(value - other.value)
     end
     
     def * other
-      begin
-        other = other.to(self.class)
-      rescue SG::Converter::NoConverterError, NoMethodError
-      end
+      other = other.to(self.class) if same_dimension?(other)
       if other.kind_of?(Unit)
-        k = self.class * other.class
-        k.new(value * other.value)
+        (self.class * other.class).new(value * other.value)
       else
         self.class.new(value * other)
       end
     end
 
     def / other
-      begin
-        other = other.to(self.class)
-      rescue SG::Converter::NoConverterError, NoMethodError
-      end
+      other = other.to(self.class) if same_dimension?(other)
       if other.kind_of?(Unit)
-        new_v = value / other.value
-        if self.class == other.class
-          new_v
-        else
-          k = self.class / other.class
-          k.new(new_v)
-        end
+        (self.class / other.class).new(value / other.value)
       else
         self.class.new(value / other)
       end
     end
+
+    def invert
+      self.class.invert.new(1 / value)
+    end
+    
+    def <=> other
+      check_dimension!(other)
+      return value <=> other.to(self.class).value if self.class === other
+      return self.to(other.class).value <=> other.value if other.class === self
+      raise TypeError.new("Incomparable: #{self.class} <=> #{other.class}")
+    end
+
+    def eql? other
+      return false if !same_dimension?(other)
+      return value.eql?(other.to(self.class).value) if self.class === other
+      return self.to(other.class).value.eql?(other.value) if other.class === self
+      raise TypeError.new("Incomparable: #{self.class}.eql?(#{other.class})")
+    end
+
+    # alias == eql?
+    def == other
+      return false if !same_dimension?(other)
+      return value == other.to(self.class).value if self.class === other
+      return self.to(other.class).value == other.value if other.class === self
+      raise TypeError.new("Incomparable: #{self.class} == #{other.class}")
+    end
+    
+    alias eq? ==
+
   end
 
   module TransformedUnit
@@ -165,11 +316,12 @@ module SG::Units
     end
 
     def self.derive base, name, abbrev = nil
-      k = Class.new(base)
-      k.include(self)
-      k.name = name
-      k.abbrev = abbrev
-      k
+      Class.new(base).tap do |u|
+        u.include(self)
+        u.name = name
+        u.abbrev = abbrev
+        u.dimension = base.dimension
+      end
     end
     
     module ClassMethods
@@ -183,6 +335,188 @@ module SG::Units
     end
   end
 
+  class Dimension
+    attr_reader :name
+    
+    def initialize name
+      #super(name)
+      @name = name
+    end
+    
+    def self.dimension?
+      self.superclass == Dimension
+    end
+
+    def self.dimension
+      if dimension?
+        return self
+      elsif superclass != Object
+        superclass.dimension
+      end
+    end
+
+    def invert
+      Per.new(NullDimension, self)
+    end
+
+    def coerce other
+      [ NullDimension, self ]
+    end
+    
+    def * other
+      # a * 1
+      return self if other == 1 || other == NullDimension
+      # a * x/a
+      return other.numerator if Per === other && self == other.denominator
+      Product.new(self, other)
+    end
+
+    def / other
+      # a / 1
+      return self if other == 1 || other == NullDimension
+      # a / 0
+      return NullDimension if other == 0 || other == NullDimension
+      # a / a
+      return NullDimension if self == other
+      # a / a/x
+      return other.denominator if Per === other && self == other.numerator
+      
+      Per.new(self, other)
+    end
+
+    def eql? other
+      Dimension === other && name == other.name
+    end
+
+    def to_s
+      name
+    end
+    
+    class Per < Dimension
+      attr_accessor :numerator, :denominator
+      def initialize n, d
+        @numerator = n
+        @denominator = d
+        super("#{n.name}/#{d.name}")
+      end
+
+      def * other
+        # a/b * b => a
+        return numerator if denominator == other
+        # a/b * x/a => x/b
+        return other.numerator / denominator if Per === other && numerator == other.denominator
+        super(other)
+      end
+
+      def / other
+        # a/b / 1/b => a
+        return numerator / other.numerator if Per === other && denominator == other.denominator
+        # a/b / a => a/b * 1/a => 1/b
+        return NullDimension / denominator if numerator == other
+        # a/b / a/x => a/b * x/a => x/b
+        return other.numerator / denominator if Per === other && numerator == other.numerator
+        super(other)
+      end
+    end
+
+    class Product < Dimension
+      attr_accessor :base, :reps
+      def initialize b, r
+        @base = b
+        @reps = r
+        super("#{b.name}*#{r.name}")
+      end
+
+      def / other
+        # a*b / b => a
+        return base if reps == other
+        # a*b / a => b
+        return reps if base == other
+        # a*b / b/x => a/x
+        return base / other.denominator if Per === other && reps == other.numerater
+        # a*b / a/x => b/x
+        return reps / other.denominator if Per === other && base == other.numerater
+        
+        super(other)
+      end
+
+      def * other
+        # a*b * x/a => x*b
+        return other.numerator * reps if Per === other && base == other.denominator
+        # a*b * x/b => a*x
+        return base * other.numerator if Per === other && reps == other.denominator
+        super(other)
+      end
+    end
+  end
+
+  class ANullDimension < Dimension
+    def invert
+      self
+    end
+
+    def * other
+      other
+    end
+
+    def / other
+      other.invert
+    end
+  end
+  
+  NullDimension = ANullDimension.new(:null)
+
+  class Unitless < Unit
+    self.dimension = NullDimension
+    self.name = ''
+    self.abbrev = ''
+
+    def + other
+      case other
+      when Unit then super(other)
+      else value + other
+      end
+    end
+    
+    def - other
+      case other
+      when Unit then super(other)
+      else value - other
+      end
+    end
+    
+    def * other
+      case other
+      when Unit then other.class.new(value * other.value)
+      else super(other) #value * other
+      end
+    end
+    
+    def / other
+      case other
+      when Unit then other.class.invert.new(value / other.value)
+      else super(other) #value / other
+      end
+    end
+    
+    def == other
+      value == other || super(other)
+    end
+    def eql? other
+      value.eql?(other) || super(other)
+    end
+
+    delegate :to_s, :to_i, :to_f, :to_r, to: :value
+
+    def coerce other
+      [ other, value ]
+    end
+
+    def inspect
+      '#<%s: %s>' % [ 'Unitless', value.inspect ]
+    end
+  end
+  
   def self.transformed_unit name, base, from_conv, to_conv = nil, abbrev: nil
     k = TransformedUnit.derive(base, name, abbrev)
     factor = from_conv
@@ -208,24 +542,6 @@ module SG::Units
     k
   end
   
-  class Dimension < SG::Algebra::Symbol
-    def initialize name
-      super(name)
-    end
-    
-    def self.dimension?
-      self.superclass == Dimension
-    end
-
-    def self.dimension
-      if dimension?
-        return self
-      elsif superclass != Object
-        superclass.dimension
-      end
-    end
-  end
-
   Length = Dimension.new(:length)
 
   class Meter < Unit
@@ -242,6 +558,7 @@ module SG::Units
 
   Inch = scaled_unit('inch', Meter, 0.0254, abbrev: 'in')
   Foot = scaled_unit('foot', Inch, 12.0)
+  Yard = scaled_unit('yard', Foot, 3.0)
   Mile = scaled_unit('mile', Foot, 5280.0)
 
   Area = Length * Length
@@ -260,10 +577,8 @@ module SG::Units
 
   class Second < Unit
     self.dimension = Time
-    
-    def self.name
-      "second"
-    end
+    self.name = 'second'
+    self.abbrev = 's'
   end
 
   Minute = scaled_unit('minute', Second, 60.0)
@@ -272,10 +587,23 @@ module SG::Units
   Week = scaled_unit('week', Day, 7.0)
   Year = scaled_unit('year', Day, 365.242)
 
+  # todo frequency as 1 / time [in a medium?]
+  Frequency = Dimension.new(:frequency)
+
+  class Hertz < Unit
+    self.dimension = Frequency
+    self.name = 'hertz'
+    self.abbrev = 'Hz'
+  end
+
+  #Hertz = transformed_unit('hertz', Second, lambda { |x| 1 / x })
+
   Temperature = Dimension.new(:temperature)
   
   class Kelvin < Unit
     self.dimension = Temperature
+    self.name = 'Kelvin'
+    self.abbrev = 'K'
   end
 
   Celsius = scaled_unit('Celsius', Kelvin, 1, 273.15)
@@ -285,25 +613,25 @@ module SG::Units
   
   class Radian < Unit
     self.dimension = Rotation
-    def self.name
-      "radian"
-    end
+    self.name = "radian"
+    self.abbrev = 'r'
   end
 
   RotUnit = scaled_unit('rotunit', Radian, Math::PI * 2)
-  Degree = scaled_unit('degree', Radian, Math::PI / 180.0)
+  Degree = scaled_unit('degree', Radian, Math::PI / 180.0, abbrev: '°')
 
+  # todo seconds -> degrees -> miles [on a planet?]
+  
   Mass = Dimension.new(:mass)
   
   class Gram < Unit
     self.dimension = Mass
-    def self.name
-      "gram"
-    end
+    self.name = 'gram'
+    self.abbrev = 'g'
   end
 
-  Ounce = scaled_unit('ounce', Gram, 28.3495)
-  PoundM = scaled_unit('pound', Ounce, 8)
+  Ounce = scaled_unit('ounce', Gram, 28.3495, abbrev: 'oz')
+  PoundM = scaled_unit('pound', Ounce, 8, abbrev: 'lbs')
   
   Velocity = Length / Time
   Acceleration = Velocity / Time
@@ -311,12 +639,11 @@ module SG::Units
 
   class Newton < Unit
     self.dimension = Force
-    def self.name
-      'newton'
-    end
+    self.name = 'newton'
+    self.abbrev = 'N'
   end
 
-  Pound = scaled_unit('pound', Newton, 4.44822)
+  Pound = scaled_unit('pound', Newton, 4.44822, abbrev: 'lbs')
 
   Count = Dimension.new(:count)
   
@@ -382,11 +709,11 @@ module SG::Units
       ronna: 1e27,
       quecto: 1e-30,
       quetta: 1e30,
-      kiba: (1<<10).to_f,
-      meba: (1<<20).to_f,
-      giba: (1<<30).to_f,
-      tiba: (1<<40).to_f,
-      peba: (1<<50).to_f
+      kiba: (1 << 10).to_f,
+      meba: (1 << 20).to_f,
+      giba: (1 << 30).to_f,
+      tiba: (1 << 40).to_f,
+      peba: (1 << 50).to_f
     }
     
     def self.si_prefix unit_s
@@ -399,15 +726,9 @@ module SG::Units
       end
     end
 
-    si_prefix :Meter
-    si_prefix :Gram
-    si_prefix :Liter
-    si_prefix :Mole
-    si_prefix :Watt
-    si_prefix :Joule
-    si_prefix :Volt
-    si_prefix :Ampere
-    si_prefix :Byte
-    si_prefix :Bit
+    %w{ Meter Second Hertz Gram
+        Liter Mole Newton Watt Joule
+        Volt Ampere Ohm Byte Bit
+    }.each(&method(:si_prefix))
   end
 end
---
+++ tests/spec/units.spec
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+require 'sg/units'
+
+using SG::Ext
+
+describe SG::Units do
+  let(:usd) { SG::Units::Unit.derive('USD', 'USD') } #'$%.2f'.to_proc)
+  let(:cents) { SG::Units.scaled_unit('cents', usd, 0.01, abbrev: 'US¢') } #'%i¢'.to_proc)
+  let(:btc) { SG::Units::Unit.derive('BTC', 'BTC') } #'$%.2f'.to_proc)
+
+  before do
+    SG::Converter.register_scaler(btc, usd, 100000)    
+  end
+
+  describe 'unit operations' do
+    it { expect(usd.name).to eq('USD') }
+    it { expect(usd.abbrev).to eq('USD') }
+    it { expect(usd.dimension.name).to eq('USD') }
+    it { expect(btc.name).to eq('BTC') }
+    it { expect(btc.abbrev).to eq('BTC') }
+    it { expect(btc.dimension.name).to eq('BTC') }
+
+    it { expect(usd * btc).to be_kind_of(Class) }
+    it { expect(usd / btc).to be_kind_of(Class) }
+    it { expect(usd / usd).to eql(SG::Units::Unitless) }
+    it { expect((usd / usd).new(3)).to eql(SG::Units::Unitless.new(3)) }
+    it { expect((usd / cents).new(10)).to be_kind_of(SG::Units::Unit::Per) }
+    it { expect(usd / btc * btc).to eql(usd) }
+    it { expect(usd * btc / btc).to eql(usd) }
+
+    it { expect(usd.invert).to eql(SG::Units::Unit::Per.derive(SG::Units::Unitless, usd)) }
+    it { expect(usd.invert.invert).to eql(usd) }
+    it { expect(usd * usd.invert).to eql(SG::Units::Unitless) }
+    it { expect(usd.invert * usd).to eql(SG::Units::Unitless) }
+  end
+
+  describe '#to_s' do
+    it { expect(usd.new(100).to_s).to eq('100 USD') }
+    it { expect(cents.new(100).to_s).to eq('100 US¢') }
+    it { expect(btc.new(100).to_s).to eq('100 BTC') }
+  end
+
+  describe 'intra-dimensional auto conversion' do
+    it { expect(usd.new(100)).to eq(cents.new(10000)) }
+    it { expect(cents.new(50)).to eq(usd.new(0.50)) }
+    it { expect(usd.new(100).eq?(btc.new(0.0003))).to eq(false) }
+  end
+
+  describe '#+' do
+    it { expect(+usd.new(10)).to eq(usd.new(10)) }
+    it { expect(usd.new(100) + usd.new(50)).to eq(usd.new(150)) }
+    it { expect(usd.new(100) + cents.new(50)).to eq(usd.new(100.50)) }
+    it { expect { usd.new(100) + btc.new(0.0003) }.
+      to raise_error(SG::Units::DimensionMismatch) }
+    it { expect { 100 + btc.new(0.0003) }.
+      to raise_error(SG::Units::DimensionMismatch) }
+    it { expect { usd.new(100) + 0.03 }.
+      to raise_error(SG::Units::DimensionMismatch) }
+  end
+
+  describe '#-' do
+    it { expect(-usd.new(10)).to eq(usd.new(-10)) }
+    it { expect(usd.new(100) - usd.new(50)).to eq(usd.new(50)) }
+    it { expect(usd.new(100) - cents.new(50)).to eq(usd.new(99.50)) }
+    it { expect { usd.new(100) - btc.new(0.0003) }.
+      to raise_error(SG::Units::DimensionMismatch) }
+    it { expect { 100 - btc.new(0.0003) }.
+      to raise_error(SG::Units::DimensionMismatch) }
+    it { expect { usd.new(100) - 0.03 }.
+      to raise_error(SG::Units::DimensionMismatch) }
+  end
+
+  describe '#*' do
+    it { expect((usd * usd).new(10)).to be_kind_of(SG::Units::Unit::Product) }
+    it { expect((usd * btc).new(10)).to be_kind_of(SG::Units::Unit::Product) }
+    it { expect(usd.new(100) * usd.new(50)).to eq((usd * usd).new(5000)) }
+    it { expect(usd.new(100) * cents.new(50)).to eq((usd * usd).new(50)) }
+    it { expect(usd.new(100) * btc.new(0.0001)).to eq((usd * btc).new(0.01)) }
+    it { expect(100 * usd.new(50)).to eq(usd.new(5000)) }
+    it { expect(usd.new(100) * 50).to eq(usd.new(5000)) }
+  end
+
+  describe '#/' do
+    it { expect((usd / usd).new(10)).to eq(10) }
+    it { expect((usd / btc).new(10)).to be_kind_of(SG::Units::Unit::Per) }
+    it { expect((usd / btc * btc).new(10)).to be_kind_of(usd) }
+    it { expect((btc * usd / btc).new(10)).to be_kind_of(usd) }
+    it { expect((btc * usd * btc / usd / btc / btc).new(10)).to be_kind_of(SG::Units::Unitless) }
+    it { expect((btc * usd * btc / btc / usd / btc).new(10)).to be_kind_of(SG::Units::Unitless) }
+    it { expect((btc * usd * btc / btc / btc / usd).new(10)).to be_kind_of(SG::Units::Unitless) }
+    it { expect(usd.new(100) / usd.new(50)).to eq(2) }
+    it { expect(usd.new(100) / cents.new(50)).to eq(200.0) }
+    it { expect(usd.new(100) / btc.new(0.0001)).to eq((usd / btc).new(1000000.0)) }
+    it { expect(usd.new(100) / btc.new(0.0001) * btc.new(10)).to eq(usd.new(10000000.0)) }
+    it { expect(btc.new(10) * usd.new(100) / btc.new(0.0001)).to eq(usd.new(10000000.0)) }
+    it { expect(100 / usd.new(50.0)).to eq(usd.invert.new(2)) }
+    it { expect(usd.new(100) / 50).to eq(usd.new(2)) }
+  end
+
+  describe '#invert' do
+    it { expect(usd.new(100.0).invert).to eql(usd.invert.new(0.01)) }
+    it { expect(usd.new(100.0).invert.invert).to eql(usd.new(100.0)) }
+  end
+end
+
+describe SG::Units::Unitless do
+  describe 'conversions' do
+    it { expect(described_class.new(5).to_s).to eq('5') }
+    it { expect(described_class.new(5).to_i).to eq(5) }
+    it { expect(described_class.new(5).to_f).to eq(5.0) }
+    it { expect(described_class.new(5).to_r).to eq(5.to_r) }
+  end
+
+  describe '#==' do
+    it { expect(described_class.new(10) == 10).to be(true) }
+    it { expect(described_class.new(5) == 10).to be(false) }
+    it { expect(10 == described_class.new(10)).to be(true) }
+    it { expect(5 == described_class.new(10)).to be(false) }
+    it { expect(described_class.new(5) == described_class.new(5)).to be(true) }
+    it { expect(described_class.new(10) == described_class.new(5)).to be(false) }
+    it { expect(described_class.new(5) == described_class.new(10)).to be(false) }
+    it { expect(described_class.new(5) == SG::Units::Inch.new(5)).to be(false) }
+    it { expect(SG::Units::Inch.new(5) == described_class.new(5)).to be(false) }
+  end
+
+  describe '#+' do
+    it { expect(described_class.new(5) + 5).to eq(10) }
+    it { expect(5 + described_class.new(5)).to eq(10) }
+    it { expect { described_class.new(5) + SG::Units::Inch.new(5) }.to raise_error(SG::Units::DimensionMismatch) }
+    it { expect { SG::Units::Inch.new(5) + described_class.new(5) }.to raise_error(SG::Units::DimensionMismatch) }
+  end
+
+  describe '#-' do
+    it { expect(described_class.new(10) - 5).to eq(5) }
+    it { expect(5 - described_class.new(2)).to eq(3) }
+    it { expect { described_class.new(5) - SG::Units::Inch.new(5) }.to raise_error(SG::Units::DimensionMismatch) }
+    it { expect { SG::Units::Inch.new(5) - described_class.new(5) }.to raise_error(SG::Units::DimensionMismatch) }
+  end
+
+  describe '#*' do
+    it { expect(described_class.new(10) * 5).to eq(50) }
+    it { expect(5 * described_class.new(2)).to eq(10) }
+    it { expect(described_class.new(10) * SG::Units::Inch.new(5)).to eq(SG::Units::Inch.new(50)) }
+    it { expect(SG::Units::Inch.new(5) * described_class.new(10)).to eq(SG::Units::Inch.new(50)) }
+  end
+
+  describe '#/' do
+    it { expect(described_class.new(10) / 5).to eq(2) }
+    it { expect(10 / described_class.new(2)).to eq(5) }
+    it { expect(described_class.new(10) / SG::Units::Inch.new(5)).to eq(SG::Units::Inch.invert.new(2)) }
+    it { expect(SG::Units::Inch.new(5) / described_class.new(10.0)).to eq(SG::Units::Inch.new(0.5)) }
+  end
+end
+
+describe SG::Units::Dimension do
+  let(:dim1) { described_class.new('dim1') }
+  let(:dim2) { described_class.new('dim2') }
+  let(:dim3) { described_class.new('dim3') }
+
+  it { expect(dim1 * dim2 / dim2).to eq(dim1) }
+  it { expect(dim1 * dim2 / dim1).to eq(dim2) }
+  it { expect(dim1 * dim2 * dim3 / dim1 / dim2).to eq(dim3) }
+  it { expect(dim1 / dim1).to eq(SG::Units::NullDimension) }
+  it { expect(1 / dim1 * dim1).to eq(SG::Units::NullDimension) }
+  it { expect(dim1.invert * dim1).to eq(SG::Units::NullDimension) }
+end
+
+describe SG::Units do
+  it { expect(SG::Units::Length).to be_kind_of(SG::Units::Dimension) }
+  %w{ Inch Foot Meter Yard Mile }.each do |unit|
+    it { expect(SG::Units.const_get(unit).dimension).to eql(SG::Units::Length) }
+  end
+
+  it { expect(SG::Units::Time).to be_kind_of(SG::Units::Dimension) }
+  %w{ Second Minute Hour Day Week Year }.each do |unit|
+    it { expect(SG::Units.const_get(unit).dimension).to eql(SG::Units::Time) }
+  end
+
+  it { expect(SG::Units::Velocity).to eql(SG::Units::Length / SG::Units::Time) }
+  it { expect(SG::Units::Acceleration).to eql(SG::Units::Velocity / SG::Units::Time) }
+  it { expect(SG::Units::Force).to eql(SG::Units::Mass * SG::Units::Acceleration) }
+  it { expect(SG::Units::Newton.dimension).to eql(SG::Units::Force) }
+  it { expect(SG::Units::Pound.dimension).to eql(SG::Units::Force) }
+  it { expect(SG::Units::Energy).to eql(SG::Units::Force * SG::Units::Length) }
+  it { expect(SG::Units::Joule.dimension).to eql(SG::Units::Energy) }
+end
