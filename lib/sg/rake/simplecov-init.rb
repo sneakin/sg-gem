3c8deada3de240808843b8472a5c2161c084dcd9
Nolan Eakins <sneakin@semanticgap.com>
2024-09-13 21:26:25 -0400

    Added paths to SimpleCov. Updated the webrick script to use new coverage path.

--- lib/sg/rake/simplecov-init.rb
+++ lib/sg/rake/simplecov-init.rb
@@ -1,4 +1,11 @@
 require 'simplecov'
 SimpleCov.start do
   coverage_dir 'doc/coverage'
+  track_files 'lib/**/*.rb'
+  track_files 'lib/*/**/*.rb'
+  track_files 'bin/*'
+  add_filter /([~]|bak)\Z/
+  add_group 'Scripts', 'bin'
+  add_group 'Libraries', 'lib'
+  add_group 'Tests', /(tests|spec)/
 end
