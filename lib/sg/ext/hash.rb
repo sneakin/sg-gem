d25ca3d2cea94780c3a94f25bee9e6b6a029a84c
Nolan Eakins <sneakin@semanticgap.com>
2024-09-15 00:47:03 -0400

    Redid Hash#symbolize_keys and #stringify* to not create an array and then a Hash.

--- lib/sg/ext/hash.rb
+++ lib/sg/ext/hash.rb
@@ -1,15 +1,24 @@
 module SG::Ext
   refine ::Hash do
     def symbolize_keys
-      self.class[self.collect { |k, v| [ k.to_sym, v ] }]
+      self.reduce(self.class.new) do |h, (k, v)|
+        h[k.to_sym] = v
+        h
+      end
     end
     
     def stringify_keys
-      self.class[self.collect { |k, v| [ k.to_s, v ] }]
+      self.reduce(self.class.new) do |h, (k, v)|
+        h[k.to_s] = v
+        h
+      end
     end
 
     def stringify_values
-      self.class[self.collect { |k, v| [ k, v.to_s ] }]
+      self.reduce(self.class.new) do |h, (k, v)|
+        h[k] = v.to_s
+        h
+      end
     end
   end
 end
