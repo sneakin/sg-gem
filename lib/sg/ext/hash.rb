3cb6649618cdcfafbb01860d13e6c5fe94937da2
Nolan Eakins <sneakin@semanticgap.com>
2024-09-15 00:49:45 -0400

    Lets document too.

--- lib/sg/ext/hash.rb
+++ lib/sg/ext/hash.rb
@@ -1,12 +1,14 @@
 module SG::Ext
   refine ::Hash do
+    # Converts the keys into symbols using #to_sym.
     def symbolize_keys
       self.reduce(self.class.new) do |h, (k, v)|
         h[k.to_sym] = v
         h
       end
     end
-    
+
+    # Converts the keys into strings using #to_s.
     def stringify_keys
       self.reduce(self.class.new) do |h, (k, v)|
         h[k.to_s] = v
@@ -14,6 +16,7 @@ module SG::Ext
       end
     end
 
+    # Converts all of the values into strings using #to_s.
     def stringify_values
       self.reduce(self.class.new) do |h, (k, v)|
         h[k] = v.to_s
