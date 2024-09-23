92f783dfba457e2cef8ce5e6e6e1c0f6427e327f
Nolan Eakins <sneakin@semanticgap.com>
2024-09-23 16:41:34 -0400

    More fun digging, picking, and plucking.

--- lib/sg/fun.rb
+++ lib/sg/fun.rb
@@ -4,7 +4,23 @@ using SG::Ext
 module SG::Fun
   Identity = lambda { |x| x }
 
-  def maker klass
+  def self.maker klass
     lambda { |*a, **o, &b| klass.new(*a, **o, &b) }
+  end
+
+  def self.digger *attrs
+    lambda { |x| x.dig(*attrs) }
+  end
+
+  def self.picker *attrs
+    lambda { |x| x.pick(*attrs) }
+  end
+
+  def self.attr_picker *attrs
+    lambda { |x| x.pick_attrs(*attrs) }
+  end
+
+  def self.plucker *attrs
+    lambda { |x| x.pluck(*attrs) }
   end
 end
