0d69c451e42026b922727e66b02ad7fba6885311
Nolan Eakins <sneakin@semanticgap.com>
2025-09-19 15:46:29 -0400

    Running meta commands for descriptions.

--- lib/sg/meta-command.rb
+++ lib/sg/meta-command.rb
@@ -1,4 +1,5 @@
 require 'sg/terminstry'
+require 'sg/table-printer'
 require 'sg/ext'
 using SG::Ext
 
@@ -11,10 +12,11 @@ class SG::MetaCommand
   # @param path The directory containing the scripts.
   # @param meta_path The path to the meta script, $0 , to exclude from the list.
   # @param tty The SG::Terminstry::Terminal for formatting.
-  def initialize path, meta_path: Self, tty: SG::Terminstry::Terminals.global
+  def initialize path, meta_path: Self, tty: SG::Terminstry::Terminals.global, banner_argument: nil
     @bin_path = Pathname.new(path).expand_path
     @meta_path = meta_path
     @tty = tty
+    @banner_argument = banner_argument
   end
 
   # Enumerates the scripts in @path into a Hash.
@@ -50,8 +52,17 @@ class SG::MetaCommand
          [ hi, normal, meta_path ])
     puts
     puts("%sCommands:%s" % [ hi, normal ])
-    [ 'help', *commands.keys ].sort.each do |name|
-      puts("  " + name)
+    cmds = commands.keys.sort.reduce([]) do |acc, name|
+      desc = nil
+      if @banner_argument
+        cmd = commands[name]
+        if cmd
+          desc = IO.popen([cmd.to_s, @banner_argument], &:readline)
+        end
+      end
+      acc << [ "  " + name, desc ]
     end
+    SG::TablePrinter.new(style: 'none').
+      print([ [ '  help', 'Print this.' ], *cmds ])
   end
 end
