1cf30537db09fcf80d30b007a256e7a70059911e
Nolan Eakins <sneakin@semanticgap.com>
2025-10-02 23:20:29 -0400

    A one of IO read helper.

--- lib/sg/io/reactor/basic_input.rb
+++ lib/sg/io/reactor/basic_input.rb
@@ -1,5 +1,27 @@
 class SG::IO::Reactor
   class BasicInput < Source
+    # @param io [IO]
+    # @param reactor [Reactor]
+    # @return [BasicInput]
+    # @yield [void]
+    # @yieldreturn [String]
+    def self.read(io, reactor: nil, &blk)
+      reactor ||= SG::IO::Reactor.current
+      rin = SG::IO::Reactor::BasicInput.new(io) do
+        begin
+          data = blk.call
+          reactor.delete(rin)
+          data
+        rescue EOFError
+          reactor.delete(rin)
+          raise
+        rescue IO::EAGAINWaitReadable
+          # reactor will try again
+        end
+      end
+      reactor << rin # todo deleting the IO requires rin since the reactor IO is unavailable
+    end
+
     def initialize io, &cb
       super(io)
       @cb = cb
